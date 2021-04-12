f7ea40809cbf4e5c7310e98ee7ed62e3418cfca6
mcorboz <melissa.corboz@bluewin.ch>
2021-04-12 21:40:36 +0200

    tentatives
    code à ne pas conserver, non fonctionnel

--- imports/ui/scripts/titre.js
+++ imports/ui/scripts/titre.js
@@ -1,3 +1,18 @@
 import { Meteor } from "meteor";
 import { Template } from "meteor/templating";
 
+Template.titre.events({
+    'click #titre'(evt){
+        // un truc qui envoie sur la page d'accueil
+        console.log("vous avez cliqué sur le titre");
+    },
+
+})
+
+Template.titre.helpers({
+    texte(){
+        const titre = "Titre";
+        console.log("coucou");
+        return titre;
+    }
+})
