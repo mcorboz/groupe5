6ca948a7716e0fc81a09468679c3a51cabb8fb9b
categorille <categorille@protonmail.com>
2021-04-13 01:16:37 +0200

    routing now works, imports are a biTch

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-import './main.html';
-import '/imports/ui/templates/App.html';
-
 import '/lib/router.js';
+// leaving out the extension here imports both .html and .js,
+// making this more readable and less error-prone
+import '/imports/ui/body';
