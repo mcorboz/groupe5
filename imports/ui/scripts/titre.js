9989f2809973a22a039a2f68a70742517ecec378
categorille <categorille@protonmail.com>
2021-04-12 23:05:33 +0200

    basic working example

--- imports/ui/scripts/titre.js
+++ imports/ui/scripts/titre.js
@@ -1,8 +1,7 @@
-import { Meteor } from "meteor";
 import { Template } from "meteor/templating";
 
 Template.titre.events({
-    'click #titre'(evt){
+    'click #titre'(event){
         // un truc qui envoie sur la page d'accueil
         console.log("vous avez cliqué sur le titre");
     },
@@ -11,8 +10,7 @@ Template.titre.events({
 
 Template.titre.helpers({
     texte(){
-        const titre = "Titre";
-        console.log("coucou");
+        const titre = "test";
         return titre;
     }
 })
