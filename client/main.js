1649b55b0c2d205b5d77218b0ecebaee6c37bffb
categorille <categorille@protonmail.com>
2021-04-13 02:13:51 +0200

    some light cleaning+commenting

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+// import the routing code or else routing (silently) isn't
+// done
 import '/lib/router.js';
 // leaving out the extension here is apparently necessary
 // or at least i haven't figured out another way, seems
