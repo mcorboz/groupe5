c23253c787b383f7c2d0cbd4f31431f38273ccbd
mcorboz <melissa.corboz@bluewin.ch>
2021-04-21 10:10:19 +0200

    Moved stuff around + created an (almost) empty template for project creation
    All imports *should* have the right path, at least it works for now

--- client/main.js
+++ client/main.js
@@ -4,4 +4,4 @@ import '/lib/router.js';
 // leaving out the extension here is apparently necessary
 // or at least i haven't figured out another way, seems
 // clean enough
-import '/imports/ui/body';
+import '/imports/ui/body.js';
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,5 +1,7 @@
 // make sure to import pretty much everything you ever use
 // here (e.g. if you're getting "No such template") errors
-import './templates/App.html';
-import './templates/Nav.html';
-import './templates/contact_info.html';
+import './App/App.html';
+import './contact_info/contact_info.html';
+import './Home_page/Home_page.html';
+import './Nav/Nav.html';
+import './Profile_page/Profile_page.html';
