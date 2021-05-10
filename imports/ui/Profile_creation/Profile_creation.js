77658d3623ad972e95b4fce460f9373c13d55b97
categorille <categorille@protonmail.com>
2021-05-10 19:38:26 +0200

    added favicon.ico, still hate imports

--- imports/ui/Profile_creation/Profile_creation.js
+++ imports/ui/Profile_creation/Profile_creation.js
@@ -1,15 +1,13 @@
 import './Profile_creation.html';
-
-
+/*
 Template.Profile_creation.helpers({
-
     username() {
         return Meteor.user().username;
     },
-    user_email(){
+    user_email() {
         return Meteor.user().email;        
     },
-    user_pronoms(){
+    user_pronoms() {
         return Meteor.user().pronoms;
-    }
-})
+    },
+}); */
