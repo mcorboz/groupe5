e32595b5ffb5e07f0d94296666167cf49dafe1a3
mcorboz <melissa.corboz@bluewin.ch>
2021-05-11 11:58:16 +0200

    added tags to object in javascript

--- imports/ui/pages/Project_new/Project_new.js
+++ imports/ui/pages/Project_new/Project_new.js
@@ -15,12 +15,32 @@ Template.Project_new.events({
         let dateStart = document.getElementById("date_start").value;
         let dateEnd = document.getElementById("date_end").value;
         let projectName = document.getElementById("project_name").value;
+
+        // récupérer les inputs technical sélectionnés
+        let checkedTechnicalTags = document.querySelectorAll('input[name="technical"]:checked');
+        // Récupérer leur valeur et les mettre dans un array
+        let technicalTags = [];
+        checkedTechnicalTags.forEach(element => {
+            technicalTags.push(element.value);
+        })
+
+        // récupérer les inputs technical sélectionnés
+        let checkedGenreTags = document.querySelectorAll('input[name="genre"]:checked');
+        // Récupérer leur valeur et les mettre dans un array
+        let genreTags = [];
+        checkedGenreTags.forEach(element => {
+            genreTags.push(element.value);
+        })
+
         // Still have to add the tags choice, which will be an array
         
+        // Créer un objet avec les informations du projet
         let nouveauProjet = {
             dateStart,
             dateEnd,
             projectName,
+            technicalTags,
+            genreTags,
         };
 
         // Appeler la méthode 'ajoutProjet' (dans Project_infoDB.js)
