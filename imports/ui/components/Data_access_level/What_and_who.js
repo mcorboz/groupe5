b3900b62f38a8556d070e71be10091c52735e051
Olipper <80388772+Olipper@users.noreply.github.com>
2021-05-12 09:44:34 +0200

    Niveau d'accès
    Code de base pour des permissions/restrictions d'accès. Pour l'instant je l'ai déposé dans "components", mais à voir si il faudra le mettre ailleurs.
    Et pour le moment, il ne sert à rien, c'est "normal" xD

---
+++ imports/ui/components/Data_access_level/What_and_who.js
@@ -0,0 +1,13 @@
+const changements = {
+    Creation: "Creation",
+    Modification: "Modification",
+    Suppression: "Suppression",
+};
+
+const levels = {
+    ADMIN: "ADMIN",
+    CREATEUR: "CREATEUR",
+    PARTICIPANT: "PARTICIPANT",
+};
+
+export {changements, levels};
