098f85f7df56f87eed0199bd9c8f6251b9e399df
mcorboz <melissa.corboz@bluewin.ch>
2021-05-19 07:39:54 +0200

    deleted stuff from pub/sub account that became useless

--- imports/ui/pages/Profile_creation/Profile_creation.js
+++ imports/ui/pages/Profile_creation/Profile_creation.js
@@ -95,34 +95,3 @@ Template.Profile_creation.events({
 	},
 });
 
-    /* 'submit .account_new'(event) {
-        // prevent default HTTP form submission
-        event.preventDefault();
-
-        // Récupérer contenu des éléments HTML
-        let pseudo = document.getElementById("pseudo").value;
-        let email = document.getElementById("email").value;
-        let password = document.getElementById("password").value;
-        let password2 = document.getElementById("password2").value;
-      
-
-        // create the account object that we will send
-        let account = {
-            pseudo,
-            email,
-            password,
-        };
-
-        Meteor.call('accounts.add', account, (err, res) => {
-            if (err) {
-                alert(err);
-            } else {
-                console.log(`Nouveau compte enregistré! ID: ${ res }`);
-                console.log(account);
-
-                // redirect the user to the profile page
-                //const params = { _id: res };
-                FlowRouter.go('Profile.show');
-            }
-        });
-    } */
--- server/main.js
+++ server/main.js
@@ -5,10 +5,6 @@ import { Projects } from '/imports/api/ProjectsCollection';
 import '/imports/api/ProjectsPublications.js';
 import '/imports/api/ProjectsMethods.js';
 
-import { Accounts } from '/imports/api/AccountsCollection';
-import '/imports/api/AccountsPublications.js';
-import '/imports/api/AccountsMethods.js';
-
 Meteor.startup(function() {
     if (Meteor.isServer) {
         console.log('Hi from the server.');
