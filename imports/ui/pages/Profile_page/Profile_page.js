5d9cf324be5776f76f8fb327b54d49a22408c77c
mcorboz <melissa.corboz@bluewin.ch>
2021-05-26 09:56:38 +0200

    début bancal de profile page + layout créer compte

--- imports/ui/pages/Profile_page/Profile_page.js
+++ imports/ui/pages/Profile_page/Profile_page.js
@@ -1,1 +1,16 @@
 import './Profile_page.html';
+
+Template.Profile_page.helpers({
+    username: function(){
+        if(Meteor.user()){
+            
+            return Meteor.user().username;
+        };
+    },
+    email: function(){
+        if(Meteor.user()) {
+            return Meteor.user().emails[0[1]];
+        };
+    },
+    
+})
