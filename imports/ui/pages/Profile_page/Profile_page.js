2cfc0dd440aac65e19384c43a392f3e73fed50d8
mcorboz <melissa.corboz@bluewin.ch>
2021-05-27 15:55:07 +0200

    left all on comment because lol

--- imports/ui/pages/Profile_page/Profile_page.js
+++ imports/ui/pages/Profile_page/Profile_page.js
@@ -1,5 +1,11 @@
 import './Profile_page.html';
 
+//console.log(Meteor.user().username); what's the problemmmmm
+//don't even know what I'm trying here anymore
+//Meteor.users.findOne( { username : FlowRouter.getParam('_username') } );
+//FlowRouter.go('Login', {_username: Meteor.user().username});
+
+
 Template.Profile_page.helpers({
     username: function(){
         if(Meteor.user()){
